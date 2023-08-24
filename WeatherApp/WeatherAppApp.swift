eb244c19122b75dbc4c7ed9d422c4f3bf505625a
Haden Olfers <haden.w.olfers@exxonmobil.com>
2023-08-24 11:19:06 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Haden Olfers on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
